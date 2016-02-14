ee69f4c436117d175c1f702424040574fe239ac9
Jeandre Rohlandt <jrohlandt@gmail.com>
2016-02-14 15:24:12 +0200

    basic calculations working

--- calculator/main.js
+++ calculator/main.js
@@ -1,7 +1,5 @@
 window.onload = function () {
-    var numberBlock = document.getElementById("number_block");
     var screen = document.getElementById("screen");
-    
     var buttons = {
         0: {value: 0, type: "number"},
         1: {value: 1, type: "number"},
@@ -22,6 +20,11 @@ window.onload = function () {
 
 
     for (index in buttons) {
+
+        if (!buttons.hasOwnProperty(index)) {
+            continue;
+        }
+
         (function () {
             var buttonObj = buttons[index];
             var buttonId = buttonObj.type+"_"+index;
@@ -32,4 +35,99 @@ window.onload = function () {
             });
         }());
     }
+
+    var calculations = {
+        division: {
+            symbol: "/",
+            func: function (int1, int2) {
+                return int1 / int2;
+            }
+        },
+        multiplication: {
+            symbol: "*",
+            func: function (int1, int2) {
+                return int1 * int2;
+            }
+        },
+        subtraction: {
+            symbol: "-",
+            func: function (int1, int2) {
+                return int1 - int2;
+            }
+        },
+        addition: {
+            symbol: "+",
+            func: function (int1, int2) {
+                return int1 + int2;
+            }
+        },
+
+    };
+
+    function calculate (int1, int2, operator) {
+        console.log(int1, int2, operator);
+
+        for (index in calculations) {
+            if (!calculations.hasOwnProperty(index)) {
+                continue;
+            }
+
+            if (calculations[index].symbol === operator) {
+                return calculations[index].func(int1, int2);
+            }
+        }
+        return false;
+    }
+
+    document.getElementById("operator_equals").addEventListener("click", function (event) {
+        event.preventDefault();
+
+        // 9*2/4+6-3
+        var sum = screen.value;
+        console.log(sum);
+
+        var characters = sum.split("");
+        console.log(characters);
+
+        var numbers = [];
+        var operators = [];
+        for (var i = 0; i < characters.length; i++) {
+            var char = characters[i];
+            //if (isNaN(parseInt(char))) {
+            //    operators.push(char);
+            //} else {
+            //    // todo handle multi char numbers
+            //    numbers.push(parseInt(char));
+            //}
+            var number = "";
+            while (!isNaN(parseInt(characters[i])) || (characters[i] === ".")) {
+                number += characters[i];
+                i++;
+            }
+
+            if (!isNaN(parseInt(number))) {
+                numbers.push(parseInt(number));
+            }
+
+            if (i >= characters.length) {
+                continue;
+            }
+
+            if (isNaN(parseInt(characters[i]))) {
+                operators.push(characters[i]);
+            }
+        }
+
+        var answer = 0;
+        for (var i = 0; i < numbers.length; i = i+2) {
+            //console.log(i);
+            var oi = operatorIterator = (i < 2) ? i : (i - 1);
+            var int1 = (i === 0) ? numbers[i] : answer;
+            var int2 = (i === numbers.length - 1) ? numbers[i] : numbers[i+1];
+            answer = calculate(int1, int2, operators[oi]);
+        }
+
+        console.log(numbers, operators, answer);
+
+    });
 };
