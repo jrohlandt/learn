ffefe4e91d4abae54305b4318d06e992a0b26f9d
Jeandre Rohlandt <jeandre@topwebdesign.co.za>
2016-11-23 13:33:31 +0200

    chapter 6

--- javascript/understanding_the_weird_parts/53_constructing_objects/59_pure_prototypal_inheritance.js
+++ javascript/understanding_the_weird_parts/53_constructing_objects/59_pure_prototypal_inheritance.js
@@ -1,3 +1,15 @@
+// polyfill for Object.create
+if (!Object.create) {
+    Object.create = function(o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation only accepts one argument/object.');
+        }
+        function F() {}
+        F.prototype = o;
+        return new F();
+    }
+}
+
 var person = {
     firstname: 'Default',
     lastname: 'Default',
